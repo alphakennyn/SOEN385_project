bc7c315d218d7393d8370f11afc2912802ce6c64
Andrew Laramee <zephix109@gmail.com>
2018-04-09 19:41:21 -0400

    Fixed star array and saved new workspace

--- SOEN385_Winer_2018_Proj/scripts/stars.m
+++ SOEN385_Winer_2018_Proj/scripts/stars.m
@@ -1,15 +1,17 @@
 function stars = stars() 
     %Get star coordinates from txt file
-    currentFile = mfilename( 'fullpath' );
     %Path currently hardcoded. With the workspace saved, we should no
     %longer need this class
     filePath = 'C:\Users\zephi\Documents\385_Project\SOEN385_project\SOEN385_Winer_2018_Proj\Datafile_StarLocationsForTelescopProject.txt';
     fileID = fopen(filePath,'r');
     formatSpec = '%d %d';
     sizeA = [3 Inf];
-    starArray = fscanf(fileID,formatSpec,sizeA);
-    stars = starArray';
+    starsArr = fscanf(fileID,formatSpec,sizeA);
+    starsArrTrans = starsArr';
+    %stars = starArray';
     fclose(fileID);
+    [~,idx] = sort(starsArrTrans(:,2)); % sort just the first row
+    stars = starsArrTrans(idx,:)';   % sort the whole matrix using the sort indices
     
     %Generates an array of 3 integer representing the coordination of the "stars"
     %{
